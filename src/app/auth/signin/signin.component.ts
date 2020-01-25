f0c2de42708fd9187c3f618d9bdf601f242e8c07
asheerrizvi <asheer.vil@gmail.com>
2020-01-25 19:20:30 +0530

    Implemented a basic form for sign-in. Need to add a service for changing between sign-in and sign-up modes.

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+  signUp =  new Subject<string>();
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSignUp() {
+    this.signUp.next('signup');
   }
 
 }
