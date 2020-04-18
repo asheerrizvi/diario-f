e79c3ce995bd9c7c44795b9dbe6190a3456f5572
Asheer Rizvi <asheer.vil@gmail.com>
2020-04-18 17:26:32 +0530

    Added the menu component for user/menu.

--- src/app/user/menu/menu.component.ts
+++ src/app/user/menu/menu.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faLayerGroup,
+  faQuoteLeft,
+  faCalendarCheck,
+  faUser,
+  faSignOutAlt,
+  faBook,
+  faSearch
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  faLayerGroup = faLayerGroup;
+  faQuoteLeft = faQuoteLeft;
+  faCalendarCheck = faCalendarCheck;
+  faUser = faUser;
+  faSignOutAlt = faSignOutAlt;
+  faBook = faBook;
+  faSearch = faSearch;
 
   constructor() { }
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,31 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import {
-  faLayerGroup,
-  faQuoteLeft,
-  faCalendarCheck,
-  faUser,
-  faSignOutAlt,
-  faBook,
-  faSearch,
-  faMapMarkerAlt,
-} from '@fortawesome/free-solid-svg-icons';
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  faLayerGroup = faLayerGroup;
-  faQuoteLeft = faQuoteLeft;
-  faCalendarCheck = faCalendarCheck;
-  faUser = faUser;
-  faSignOutAlt = faSignOutAlt;
-  faBook = faBook;
-  faSearch = faSearch;
-  faMapMarkerAlt = faMapMarkerAlt;
-
   constructor() {}
 
   ngOnInit() {}
